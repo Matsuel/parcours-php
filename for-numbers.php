3aa266a54d545a3e767b60bcedd90ca543b4593c
Matsuel <matsuel0308@gmail.com>
2023-11-23 10:37:11 +0100

    for-numbers

--- for-numbers.php
+++ for-numbers.php
@@ -2,7 +2,7 @@
 $rep="";
 for ($i=2; $i<=100;$i++){
     $prime= true;
-    for ($j=2; $j<$i/2; $j++){
+    for ($j=2; $j<=$i/2; $j++){
         if ($i%$j==0){$prime=false;}
     }
     if ($prime){
