391b4a330c511c55e5cf6f195334416df79c719b
Matsuel <matsuel0308@gmail.com>
2023-11-23 16:40:24 +0100

    check-the-robot-circuits.php

--- check-the-robot-circuits.php
+++ check-the-robot-circuits.php
@@ -3,12 +3,12 @@
 function checkCircuits(int $nb):array{
     $rep=[];
     $arrayobj=[
-        2=>"Left arm",
-        3=>"Right arm",
-        5=>"Motherboard",
-        7=>"Processor",
-        11=>"Zip defluxer",
-        13=>"Motor"
+        2=>'Left arm',
+        3=>'Right arm',
+        5=>'Motherboard',
+        7=>'Processor',
+        11=>'Zip defluxer',
+        13=>'Motor'
     ];
     foreach ($arrayobj as $key=>$value){
         if ($nb%$key==0){
@@ -17,3 +17,5 @@ function checkCircuits(int $nb):array{
     }
     return $rep;
 }
+
+print_r(checkCircuits(825));
