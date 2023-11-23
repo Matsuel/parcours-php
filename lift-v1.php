f18d89eade471281c817aaec37b1b4d1be99f5fa
Matsuel <matsuel0308@gmail.com>
2023-11-23 14:02:34 +0100

    liftv1

--- lift-v1.php
+++ lift-v1.php
@@ -3,7 +3,11 @@
 function getFloor(int $elevator, ?int $requestedFloor, array $listOfButtonsCalled): ?int
 {
     if ($requestedFloor==null){
-        return $listOfButtonsCalled[0];
+        if (count($listOfButtonsCalled)>1 && $listOfButtonsCalled[0]<0){
+            return $listOfButtonsCalled[1];
+        }else{
+            return $listOfButtonsCalled[0];
+        }
     }else{
         return $requestedFloor;
     }
@@ -19,3 +23,5 @@ function getDirection(int $elevator, ?int $requestedFloor, array $listOfButtonsC
         return 0;
     }
 }
+
+echo getFloor(0, null, [-3, 2]);
