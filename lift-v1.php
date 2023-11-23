7f7b588fc9632a33cc5034542d05a50645d48f26
Matsuel <matsuel0308@gmail.com>
2023-11-23 13:50:36 +0100

    liftv1

---
+++ lift-v1.php
@@ -0,0 +1,31 @@
+<?php
+
+function getFloor(int $elevator, int $requestedFloor, array $listOfButtonsCalled)
+{
+    if ($requestedFloor==null){
+        return $listOfButtonsCalled[0];
+    }else{
+        return $requestedFloor;
+    }
+}
+
+function getDirection(int $elevator, int $requestedFloor, array $listOfButtonsCalled):int
+{
+    if ($requestedFloor==null){
+        if ($elevator<$listOfButtonsCalled[0]){
+            return 1;
+        }elseif ($elevator==$listOfButtonsCalled[0]){
+            return 0;
+        }else{
+            return -1;
+        }
+    }else{
+        if ($elevator<$requestedFloor){
+            return 1;
+        }elseif ($elevator==$requestedFloor){
+            return 0;
+        }else{
+            return -1;
+        }
+    }
+}
