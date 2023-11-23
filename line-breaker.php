2dc5668d18ca0a16e3dbb69bea805513f8752532
Matsuel <matsuel0308@gmail.com>
2023-11-23 15:16:52 +0100

    line-breaker

--- line-breaker.php
+++ line-breaker.php
@@ -1,60 +1,60 @@
 <?php
 
-// function breakLines(string $string, int $length): string
-// {
-//     if(strlen($string)==$length){
-//         return $string;
-//     }elseif (strlen($string)> $length){
-//         $wordSize=0;
-//         for($i=0;$i<strlen($string);$i++){
-//             if($string[$i]=="\n"){
-//                 $wordSize=1;
-//             }elseif ($wordSize==$length){
-//                 $nbCharSpace=0;
-//                 while($string[$i-$nbCharSpace]!=" "){
-//                     $nbCharSpace++;
-//                 }
-//                 $string[$i-$nbCharSpace]="\n";
-//                 $wordSize=0;
-//             }elseif($wordSize!=$length){
-//                 $wordSize++;
-//             }
-//         }
-//         return $string;
-//     }
-// }
-
-function breakLines(string $string, int $length):string
+function breakLines(string $string, int $length): string
 {
-    $rep="";
     if(strlen($string)==$length){
         return $string;
     }elseif (strlen($string)> $length){
-        for($i=0; $i<=strlen($string);$i++){
-            if($i%$length==0 && $i>1){
-                if($string[$i]!=" "){
-                    $a=0;
-                    while($string[$i-$a]!=" "){
-                        $a++;
-                    }
-                    $string[$i-$a]="\n";
+        $wordSize=0;
+        for($i=0;$i<strlen($string);$i++){
+            if($string[$i]=="\n"){
+                $wordSize=1;
+            }elseif ($wordSize==$length){
+                $nbCharSpace=0;
+                while($string[$i-$nbCharSpace]!=" "){
+                    $nbCharSpace++;
                 }
-                if($string[$i]==" "){
-                    $rep.="\n";
-                }elseif ($string[$i+1]==" "){
-                    $rep.= $string[$i]."\n";
-                    $i++;
-                }
-                else{
-                    $rep.= $string[$i]."\n";
-                }
-            }else{
-                $rep.= $string[$i];
+                $string[$i-$nbCharSpace]="\n";
+                $wordSize=0;
+            }elseif($wordSize!=$length){
+                $wordSize++;
             }
         }
+        return $string;
     }
-    return $rep;
 }
+
+// function breakLines(string $string, int $length):string
+// {
+//     $rep="";
+//     if(strlen($string)==$length){
+//         return $string;
+//     }elseif (strlen($string)> $length){
+//         for($i=0; $i<=strlen($string);$i++){
+//             if($i%$length==0 && $i>1){
+//                 if($string[$i]!=" "){
+//                     $a=0;
+//                     while($string[$i-$a]!=" "){
+//                         $a++;
+//                     }
+//                     $string[$i-$a]="\n";
+//                 }
+//                 if($string[$i]==" "){
+//                     $rep.="\n";
+//                 }elseif ($string[$i+1]==" "){
+//                     $rep.= $string[$i]."\n";
+//                     $i++;
+//                 }
+//                 else{
+//                     $rep.= $string[$i]."\n";
+//                 }
+//             }else{
+//                 $rep.= $string[$i];
+//             }
+//         }
+//     }
+//     return $string;
+// }
 
 echo breakLines('Line with words', 15);
 
