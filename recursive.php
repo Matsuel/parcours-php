933184c02b1e95b0d3c27a44d9b8e6450ec7d5b5
Matsuel <matsuel0308@gmail.com>
2023-11-23 14:23:02 +0100

    recursive

--- recursive.php
+++ recursive.php
@@ -2,11 +2,15 @@
 
 function factorial(?int $number):float
 {
-    if ($number<=2){
+    if ($number==1){
+        return 1;
+    }elseif ($number<=2){
         return $number;
     }else{
         return (int) $number * factorial($number-1);
     }
 }
 
+echo factorial(1)."\n";
+echo factorial(5)."\n";
 echo  factorial(45);
