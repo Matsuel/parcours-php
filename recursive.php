7ac7ce93de1960ced8571e4ecd47dc97a7fdb9f0
Matsuel <matsuel0308@gmail.com>
2023-11-23 14:15:31 +0100

    recursive

--- recursive.php
+++ recursive.php
@@ -1,10 +1,10 @@
 <?php
 
-function factorial(?int $number):?int
+function factorial(?int $number):int
 {
     if (intval($number)==1){
         return 1;
     }else{
-        return (int) $number * factorial($number-1);
+        return intval($number * factorial($number-1));
     }
 }
