34bc1a66ad33f1546228276996408423cf33b90e
Matsuel <matsuel0308@gmail.com>
2023-11-23 15:49:02 +0100

    join

--- join.php
+++ join.php
@@ -1,13 +1,13 @@
 <?php
 
-function joinWords(array $words,string $joiner=","): string {
+function joinWords(array $words,string $pattern=","): string {
     $result = "";
-    $joiner = $joiner . " ";
+    $pattern = $pattern . " ";
     for ($i = 0; $i < count($words); $i++) {
         if ($i == count($words) - 1) {
             $result .= $words[$i];
         } else {
-            $result .= $words[$i] . $joiner;
+            $result .= $words[$i] . $pattern;
         }
     }
     return $result;
