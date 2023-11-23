1efa733abdfe8ac6f46f3e926808abfd7109a26a
Matsuel <matsuel0308@gmail.com>
2023-11-23 16:34:38 +0100

    manage-robot-movements.php

--- manage-robot-movements.php
+++ manage-robot-movements.php
@@ -9,9 +9,14 @@ function manageMovements(string $movements):array{
     ];
     $rep=array();
     for ($i=0; $i<strlen($movements);$i++){
-        array_push($rep, $arrayMovements[$movements[$i]]);
+        if (in_array($arrayMovements[$movements[$i]], $rep)){
+            $arrayMovements[$movements[$i]] = $arrayMovements[$movements[$i]]." AGAIN";
+            array_push($rep, $arrayMovements[$movements[$i]]);
+        }else{
+            array_push($rep, $arrayMovements[$movements[$i]]);
+        }
     }
     return $rep;
 }
 
-print_r(manageMovements("RLFB"));
+print_r(manageMovements("RRLFB"));
