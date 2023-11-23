9e189591bec4e7a21e53e5d44c46c0823d5d7fce
Matsuel <matsuel0308@gmail.com>
2023-11-23 14:51:25 +0100

    find-in-recursively

---
+++ find-in-recursively.php
@@ -0,0 +1,34 @@
+<?php
+
+function findIn(string $toFind, array $array): string
+{
+    foreach($array as $key=> $value){
+        if ($key==$toFind){
+            return $value;
+        }elseif (is_array($value)){
+            $result= findIn($toFind, $value);
+            if ($result!="false"){
+                return $result;
+            }
+        }
+    }
+    return "false";
+}
+
+$tab = [
+    "name" => "forIn",
+    "type" => "function",
+    "arguments" => [
+        "firstParam" => [
+            "paramType" => "string",
+            "description" => "the value key to find"
+        ],
+        "secondParam" => "array"
+    ],
+    "return" => "string or bool"
+];
+
+echo findIn('name', $tab)."\n";
+echo findIn('description', $tab)."\n";
+echo findIn('secondParam', $tab)."\n";
+echo findIn('yeah', $tab);
