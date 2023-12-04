c44debe640c9849107059f014776c7bef734ade6
Matsuel <matsuel0308@gmail.com>
2023-12-04 15:13:10 +0100

    this is magic

---
+++ this-is-magic.php
@@ -0,0 +1,35 @@
+<?php
+
+class Magic 
+{
+    public function __constructor()
+    {
+        echo "I'm a constructor";
+    }
+
+    public function __destruct()
+    {
+        echo "I'm a destructor";
+    }
+
+    public function __get($name)
+    {
+        echo "You are trying to get $name";
+    }
+
+    public function __set($name, $value)
+    {
+        echo "You are trying to set $name to $value";
+    }
+
+    public function __isset($name)
+    {
+        echo "You are trying to check if $name is set";
+    }
+
+    public function __toString()
+    {
+        return "I'm a string";
+    }
+
+}
