9ec6fa75001f2b89847fc98779fe5ade4c9ae830
Matsuel <matsuel0308@gmail.com>
2023-12-04 13:46:08 +0100

    map callable

--- map-this-callable.php
+++ map-this-callable.php
@@ -23,3 +23,5 @@ print_r(myArrayMap(null, [1, 2, 3], ['one', 'two', 'three'], ['uno', 'dos', 'tre
 print_r(myArrayMap(static fn ($n) => $n * $n * $n, [1, 2, 3 ,4 ,5]));
 
 print_r(myArrayMap(null, [1, 3, 7]));
+
+print_r(myArrayMap(static fn ($n) => $n['value'], ['value1' => 1, 'value2' => 2, 'value3' => 3]));
