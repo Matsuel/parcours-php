ba80df5ebbc83e0789213d96169b19c26de7d598
Matsuel <matsuel0308@gmail.com>
2023-12-04 15:03:33 +0100

    array manip

---
+++ array-manipulator.php
@@ -0,0 +1,26 @@
+<?php
+
+function reverse(array $array): array
+{
+    return array_reverse($array);
+}
+
+function push(array $array, string... $string): int
+{
+    return array_push($array, ...$string);
+}
+
+function sum(array $array): int
+{
+    return array_sum($array);
+}
+
+function arrayContains(array $array, int|string|float $value): mixed
+{
+    return in_array($value, $array);
+}
+
+function merge(array, array, array): array
+{
+    return array_merge($array, ...$array);
+}
