8953215bbbf8f3900647d8e22cf34c1965c14957
Matsuel <matsuel0308@gmail.com>
2023-12-04 15:25:17 +0100

    distance

---
+++ distance-calculator.php
@@ -0,0 +1,17 @@
+<?php
+
+class Geolocation 
+{
+    public static function fromGeoPoints(float $lat1, float $lon1, float $lat2, float $lon2): float{
+        $earthRadius = 6371;
+        $dLat = deg2rad($lat2 - $lat1);
+        $dLon = deg2rad($lon2 - $lon1);
+        $a = sin($dLat / 2) * sin($dLat / 2) + cos(deg2rad($lat1)) * cos(deg2rad($lat2)) * sin($dLon / 2) * sin($dLon / 2);
+        $c = 2 * asin(sqrt($a));
+        return round($earthRadius * $c,1);
+    }
+}
+
+$calc = Geolocation::fromGeoPoints(40.76, -73.984, 38.89, -77.032);
+
+echo $calc; // 334.436
