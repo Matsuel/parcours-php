242bf36fbb140ed7fe0877fdd1f1a0da4d9857ce
Matsuel <matsuel0308@gmail.com>
2023-12-04 11:43:01 +0100

    map callable

--- map-this-callable.php
+++ map-this-callable.php
@@ -2,6 +2,9 @@
 
 function myArrayMap(?callable $callback, array $array, array ...$arrays):array{
     $result=[];
+    if ($callback==null){
+        $callback=function($value){return $value;};
+    }
     foreach ($array as $key=>$value){
         $result[$key]=$callback($value);
     }
