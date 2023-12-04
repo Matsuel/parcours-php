da9973d97907520e4d9fdf17042c5605aca44935
Matsuel <matsuel0308@gmail.com>
2023-12-04 13:50:47 +0100

    i want a cocktail

---
+++ i-want-a-cocktail.php
@@ -0,0 +1,13 @@
+<?php
+
+class Mojito{
+    public float $alcoholRate = 0.15;
+    public array $ingredients = ['rum', 'lime', 'sparkling water', 'mint', 'sugar'];
+    public int $price =8;
+
+    public function __construct(float $alcoholRate, array $ingredients, int $price){
+        $this->alcoholRate = $alcoholRate;
+        $this->ingredients = $ingredients;
+        $this->price = $price;
+    }
+}
