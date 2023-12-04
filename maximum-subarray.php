58992d00edcb89d009a50599e777906df507562a
Matsuel <matsuel0308@gmail.com>
2023-12-04 10:37:04 +0100

    subarray max

--- maximum-subarray.php
+++ maximum-subarray.php
@@ -3,9 +3,11 @@
 function findMaximumSubarray (array $array): int | float{
     $sum=0;
     for ($i=0;$i < count($array);$i++){
-        if ($sum+$array[$i] > $sum){
+        if ($sum+$array[$i] > $sum || $sum+$array[$i] == 0){
             $sum+=$array[$i];
         }
     }
     return $sum;
 }
+
+echo findMaximumSubarray([1, -1, 4, 6, 3]);
