62980ce619c13d2a7bb5aaa75ace8f4cf62b5a76
Matsuel <matsuel0308@gmail.com>
2023-12-04 14:00:17 +0100

    car

---
+++ many-fuel.php
@@ -0,0 +1,30 @@
+<?php
+
+class Car 
+{
+    private float $tank;
+
+    public function __construct()
+    {
+        $this->tank = 0;
+    }
+
+    public function getTank(): float
+    {
+        return $this->tank;
+    }
+
+    public function setTank(float $tank): void
+    {
+        $this->tank += $tank;
+    }
+
+    public function ride(float $distance){
+        $consume= $distance/0.5;
+        $this->tank -= $consume;
+        if($this->tank < 0){
+            $this->tank = 0;
+        }
+        return $consume;
+    }
+}
