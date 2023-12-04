75e7cb4486b331028daa6b04b9a0f4b7be7c4f62
Matsuel <matsuel0308@gmail.com>
2023-12-04 14:20:45 +0100

    call map

--- map-this-callable.php
+++ map-this-callable.php
@@ -1,27 +1,20 @@
 <?php
-function myArrayMap(?callable $callback, array $array, array ...$arrays): array {
-    $result = [];
-    if ($callback == null && count($arrays) > 0) {
-        $callback = function($value) { return $value; };
-        foreach ($array as $key => $value) {
-            $result[$key] = array_merge([$value], array_column($arrays, $key));
-        }
-        return $result;
-    }else if ($callback == null && count($arrays) == 0){
+function myArrayMap(?callable $callback, array $array): array {
+    if(!$callback){
         return $array;
-    }else{
-        foreach ($array as $key => $value) {
-            $result[$key] = $callback($value, ...array_column($arrays, $key));
-        }
-        return $result;
     }
+    $result = [];
+    foreach($array as $key => $value){
+        $result[$key] = $callback($value);
+    }
+    return $result;
     
 }
 
-print_r(myArrayMap(null, [1, 2, 3], ['one', 'two', 'three'], ['uno', 'dos', 'tres']));
+// print_r(myArrayMap(null, [1, 2, 3], ['one', 'two', 'three'], ['uno', 'dos', 'tres']));
 
-print_r(myArrayMap(static fn ($n) => $n * $n * $n, [1, 2, 3 ,4 ,5]));
+// print_r(myArrayMap(static fn ($n) => $n * $n * $n, [1, 2, 3 ,4 ,5]));
 
-print_r(myArrayMap(null, [1, 3, 7]));
+// print_r(myArrayMap(null, [1, 3, 7]));
 
 print_r(myArrayMap(static fn ($n) => $n['value'], ['value1' => 1, 'value2' => 2, 'value3' => 3]));
