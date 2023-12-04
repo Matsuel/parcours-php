37dff22b15a5073e9d9577130c859e10057496ee
Matsuel <matsuel0308@gmail.com>
2023-12-04 09:51:18 +0100

    dna diff

---
+++ dna-difference.php
@@ -0,0 +1,15 @@
+<?php 
+
+function dnaDiff(string $dna1, string $dna2):bool|int{
+    if (strlen($dna1)!=strlen($dna2)){
+        return false;
+    }else{
+        $diff=0;
+        for ($i=0; i<strlen($dna1);$i++){
+            if ($dna1[$i]!=$dna2[$i]){
+                $diff++;
+            }
+        }
+        return $diff;
+    }
+}
