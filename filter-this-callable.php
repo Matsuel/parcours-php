ab67b095c288c8cbbd452e7baf93db0a54525605
Matsuel <matsuel0308@gmail.com>
2023-12-04 14:35:27 +0100

    filter map

--- filter-this-callable.php
+++ filter-this-callable.php
@@ -1,17 +1,17 @@
 <?php
 
-function myArrayFilter(array $array, ?callable $callback=null, int $mode=0):array{
+function myArrayFilter(array $array, ?callable $callback=null):array{
     $result = [];
     if($callback == null){
-        foreach ($array as $key => $value) {
+        foreach($array as $key => $value){
             if($value){
                 $result[$key] = $value;
             }
         }
         return $result;
     }else{
-        foreach ($array as $key => $value) {
-            if($callback($value, $key, $array)){
+        foreach($array as $key => $value){
+            if($callback($value)){
                 $result[$key] = $value;
             }
         }
