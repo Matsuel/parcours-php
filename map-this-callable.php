a0a94b3d8c9163c4ce76b1336e217cc718af9de5
Matsuel <matsuel0308@gmail.com>
2023-12-04 11:54:31 +0100

    map callable

--- map-this-callable.php
+++ map-this-callable.php
@@ -3,11 +3,19 @@ function myArrayMap(?callable $callback, array $array, array ...$arrays): array 
     $result = [];
     if ($callback == null) {
         $callback = function($value) { return $value; };
+        foreach ($array as $key => $value) {
+            $result[$key] = array_merge([$value], array_column($arrays, $key));
+        }
+        return $result;
+    }else{
+        foreach ($array as $key => $value) {
+            $result[$key] = $callback($value, ...array_column($arrays, $key));
+        }
+        return $result;
     }
-    foreach ($array as $key => $value) {
-        $result[$key] = array_merge([$value], array_column($arrays, $key));
-    }
-    return $result;
+    
 }
 
 print_r(myArrayMap(null, [1, 2, 3], ['one', 'two', 'three'], ['uno', 'dos', 'tres']));
+
+print_r(myArrayMap(static fn ($n) => $n * $n * $n, [1, 2, 3 ,4 ,5]));
