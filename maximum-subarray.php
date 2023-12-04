5dc45a43ed4ce5121edf2dcdacd13ea6bd77b97b
Matsuel <matsuel0308@gmail.com>
2023-12-04 10:56:54 +0100

    subarray max

--- maximum-subarray.php
+++ maximum-subarray.php
@@ -1,13 +1,16 @@
 <?php
 
 function findMaximumSubarray (array $array): int | float{
-    $sum=0;
-    for ($i=0;$i < count($array);$i++){
-        if ($sum+$array[$i] > $sum || $sum+$array[$i] == 0){
-            $sum+=$array[$i];
-        }
+    if (count($array)==0) return 0;
+    $sum=$array[0];
+    $currentSum = $array[0];
+    for ($i=1;$i < count($array);$i++){
+        $currentSum = max($array[$i], $currentSum + $array[$i]);
+        $sum = max($sum, $currentSum);
     }
     return $sum;
 }
 
-echo findMaximumSubarray([1, -1, 4, 6, 3]);
+echo findMaximumSubarray([1, -1, 4, 6, 3]) . "\n";
+
+echo findMaximumSubarray([-2, 1, -3, 4, -1, 2, 1, -5, 4]);
