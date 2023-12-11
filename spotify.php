3a8e9dca215943f55a2a7078fb1f5a24823a20d3
Matsuel <matsuel0308@gmail.com>
2023-12-11 10:04:30 +0100

    spotify

--- spotify.php
+++ spotify.php
@@ -38,7 +38,7 @@ class Song{
 
 class Playlist{
     private array $songs;
-    private int $totalMedias;
+    private int $totalMedias=0;
 
     public function addMedia(Song $song): void{
         $this->songs[] = $song;
@@ -47,12 +47,12 @@ class Playlist{
 
     public function __toString():string{
         $rep="";
-        $rep+="Songs added: "+this.totalMedias+"\nPlaylist Length:";
+        $rep.="Songs added: ".strval($this->totalMedias)."\nPlaylist Length:";
         $duration=0;
         foreach($this->songs as $song){
-            $duration+=intval($song->getDuration().explode(":",$song->getDuration())[1])+intval(explode(":",$song->getDuration())[0])*60;
+            $duration+=intval(explode(":",$song->getDuration())[0])*60+intval(explode(":",$song->getDuration())[1]);
         }
-        $rep+= ($duration%3600)+"h "+($duration%3600)/60+"min "+($duration%3600)%60+"sec";
+        $rep .=" ".strval(intval($duration/3600))."h ". strval(intval($duration/60))."m ".strval($duration%60)."s\n";
         return $rep;
     }
 }
@@ -68,4 +68,42 @@ class App{
         $this->content = $content;
     }
 
+    public function start(){
+        $playlist=new Playlist();
+        foreach($this->readLine(true) as $line){
+            $song=explode(";",$line);
+            $playlist->addMedia(new Song($song[0],$song[1],$song[2]));
+        }
+        echo $playlist->__toString();
+    }
+
+    private function readLine(bool $asArray = false): array|bool|string
+    {
+        ob_start();
+
+        echo implode("", $this->getContent());
+
+        $data = ob_get_contents();
+        if ($asArray) {
+            $data = explode("\n", ob_get_contents());
+        }
+
+        ob_clean();
+
+        return $data;
+    }
+
+    public function write(string $content): void
+    {
+        $this->content[] = $content;
+    }
+
 }
+
+$app = new App();
+$app->setContent(["ABBA;Mamma Mia;3:35\n", "Harry Styles;Watermelon Sugar;2:54\n", "LF SYSTEM;Affraid To Feel;2:58"]);
+$app->start();
+
+$app2 = new App();
+$app2->setContent(["JUL;Alors la zone;3:25\n", "Naps;La kiffance;2:59\n", "2TH;Si seulement;4:09\n", "Vayn;24H chrono;3:48"]);
+$app2->start();
