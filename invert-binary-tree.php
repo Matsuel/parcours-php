33897851811edfd03ec9a18cf727a56db7327482
Matsuel <matsuel0308@gmail.com>
2023-12-11 09:36:17 +0100

    invert

--- invert-binary-tree.php
+++ invert-binary-tree.php
@@ -1,17 +1,6 @@
 <?php
 
-// Data tree
-class BinaryNode
-{
-    public ?int $value = null; // Node value
-    public ?BinaryNode $left = null; // Left child
-    public ?BinaryNode $right = null; // Right child
 
-    public function __construct(int $value)
-    {
-        $this->value = $value;
-    }
-}
 
 function invertTree (BinaryNode|null $binary): BinaryNode|null{
     if ($binary === null) {
@@ -27,20 +16,3 @@ function invertTree (BinaryNode|null $binary): BinaryNode|null{
     return $binary;
 }
 
-// Basic use
-$root = new BinaryNode(1);
-$rootLeftChild = new BinaryNode(2);
-$rootRightChild = new BinaryNode(3);
-
-$root->left = $rootLeftChild;
-$root->right = $rootRightChild;
-
-echo $root->value; // 1
-echo $root->left->value; // 2
-echo $root->right->value; // 3
-
-$invertedTree = invertTree($root);
-
-echo $invertedTree->value; // 1
-echo $invertedTree->left->value; // 3
-echo $invertedTree->right->value; // 2
