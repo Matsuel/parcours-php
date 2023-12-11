51488366858ba18515b4602fe694814725b06f31
Matsuel <matsuel0308@gmail.com>
2023-12-11 09:15:25 +0100

    swimming pool

---
+++ heat-my-swimming-pool.php
@@ -0,0 +1,35 @@
+<?php
+
+interface PoolTempsInterface {
+    function getActualTemp(): int;
+    function getLastDaysTemps(): array;
+    function setHeater(bool $value): void;
+}
+
+class PoolTemps implements PoolTempsInterface {
+    public $isActive = false;
+    public $currentTemp;
+    public $averageTemps;
+
+    public function __construct(int $currentTemp, array $averageTemps){
+        $this->currentTemp = $currentTemp;
+        $this->averageTemps = $averageTemps;
+    }
+
+    public function getActualTemp(): int {
+        return $this->currentTemp;
+    }
+
+    public function getLastDaysTemps(): array {
+        return $this->averageTemps;
+    }
+
+    public function setHeater(bool $value): void {
+        $this->isActive = $value;
+    }
+
+    public function activateHeater(): void {
+        $moyenne = array_sum($this->getLastDaysTemps()) / count($this->getLastDaysTemps());
+        $moyenne <20 && $this->getActualTemp()<25 ? $this->setHeater(true) : $this->setHeater(false);
+    }
+}
